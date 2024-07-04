22babeb9543f3c0ad4f4d65660860d27f332e334
Luka Khutsishvili <lukakhutsishvili777@gmail.com>
2024-07-04 19:11:45 +0400

    add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,68 @@
+import styled from "styled-components";
+import Redberry from "../../public/images/LOGO-02 3.png";
+import BackgroundImage from "/images/shutterstock_2189773743 1.png";
+import Agency from "/images/LOGO-02 3.png";
+import { Link } from "react-router-dom";
 
-function Home() {
+export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <Background>
+      <div className="redberry-image">
+        <img src={Redberry} alt="Redberry Logo" />
+      </div>
+      <div className="center-div">
+        <Link to={"/PersonalInfo"}>
+          <button>ᲠᲔᲖᲘᲣᲛᲔᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ</button>
+        </Link>
+        <img src={Agency} alt="Agency Logo" />
+      </div>
+    </Background>
+  );
 }
 
-export default Home
+const Background = styled.div`
+  background: url(${BackgroundImage}) no-repeat center center fixed;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  padding: 0 4.36rem;
+  display: flex;
+  flex-direction: column;
+
+  .redberry-image {
+    width: 100%;
+    border-bottom: 0.0625rem solid #1a1a1a;
+    img {
+      padding-top: 1.57rem;
+      padding-bottom: 1.63rem;
+    }
+  }
+
+  .center-div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+    position: relative;
+
+    img {
+      position: absolute;
+      right: 270px;
+      bottom: 130px;
+    }
+
+    button {
+      width: 29rem;
+      font-size: 1.25rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+      padding: 1.12rem 0;
+      border-radius: 0.5rem;
+      background: var(--off-black, #1a1a1a);
+      color: white;
+      border: none;
+      cursor: pointer;
+    }
+  }
+`;
--- src/pages/PersonalInfo.tsx
+++ src/pages/PersonalInfo.tsx
@@ -1,6 +1,274 @@
+import styled from "styled-components";
 
-function PersonalInfo() {
-  return <div>PersonalInfo</div>;
+import { Link, useNavigate } from "react-router-dom";
+import {
+  FormProvider,
+  useForm,
+  useFieldArray,
+  useWatch,
+} from "react-hook-form";
+import { useContext, useEffect, useState } from "react";
+import { CvContext, CvData } from "../App";
+import CVcomponent from "../components/CVcomponent";
+import Header from "../components/personalInfo/Header";
+import Input from "../components/experience/Input";
+type formTypes = {
+  experience: {
+    position: string;
+    employer: string;
+    startingDate: string;
+    finishingDate: string;
+    description: string;
+  }[];
+};
+
+export default function PersonalInfo() {
+  const [experienceInfo, setExperienceInfo] = useState<formTypes>();
+  const navigate = useNavigate();
+  const { setExperienceCv, experienceCv } = useContext(CvContext);
+  const methods = useForm<formTypes>({
+    defaultValues: {
+      experience: [
+        {
+          position: "",
+          employer: "",
+          startingDate: "mm/dd/yyyy",
+          finishingDate: "mm/dd/yyyy",
+          description: "",
+        },
+      ],
+    },
+  });
+
+  const submit = (data: formTypes) => {
+    setExperienceInfo(data);
+    navigate("/Education");
+  };
+
+  const {
+    handleSubmit,
+    control,
+    register,
+    watch,
+    formState: { submitCount, errors },
+  } = methods;
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "experience",
+  });
+
+  const getBorderColor = (index: number) => {
+    const value = watch(`experience.${index}.description`);
+    const hasSubmitted = submitCount > 0;
+    const error = errors?.experience?.[index]?.description;
+
+    if (hasSubmitted) {
+      if (error) {
+        return "#ef5050";
+      } else if (value) {
+        return "#98e37e";
+      }
+    }
+    return "#bcbcbc";
+  };
+
+  const values = useWatch({ control, name: `experience` });
+  useEffect(() => {
+    setExperienceCv(values as CvData);
+  }, [values, setExperienceCv]);
+  console.log(experienceInfo);
+  useEffect(() => {
+    const savedData = localStorage.getItem("experienceFormData");
+    if (savedData) {
+      methods.reset(JSON.parse(savedData));
+    }
+  }, []);
+
+  useEffect(() => {
+    const subscription = methods.watch((value) => {
+      localStorage.setItem("experienceFormData", JSON.stringify(value));
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [methods, setExperienceCv, experienceCv]);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(submit)}>
+          <MainDiv>
+            <Header />
+            <section style={{ marginTop: "77px", paddingLeft: "102px" }}>
+              {fields.map((item, index) => (
+                <div
+                  key={item.id}
+                  style={{
+                    display: "flex",
+                    gap: "31px",
+                    flexDirection: "column",
+                    marginTop: index !== 0 ? "40px" : "0",
+                  }}
+                >
+                  <Input
+                    type="text"
+                    labelTxt="თანამდებობა"
+                    errorTxt="მინუმუმ 2 სიმბოლო"
+                    name={`experience.[${index}].position`}
+                  >
+                    დეველოპერი, დიზაინერი, ა.შ.
+                  </Input>
+                  <Input
+                    type="text"
+                    labelTxt="დამსაქმებელი"
+                    errorTxt="მინუმუმ 2 სიმბოლო"
+                    name={`experience.[${index}].employer`}
+                  >
+                    დამსაქმებელი
+                  </Input>
+                  <div style={{ display: "flex", gap: "56px" }}>
+                    <div style={{ width: "calc(50% - 23px)" }}>
+                      <Input
+                        type="date"
+                        labelTxt="დაწყების რიცხვი"
+                        name={`experience.[${index}].startingDate`}
+                      ></Input>
+                    </div>
+                    <div style={{ width: "calc(50% - 23px)" }}>
+                      <Input
+                        type="date"
+                        labelTxt="დაწყების რიცხვი"
+                        name={`experience.${index}.finishingDate`}
+                      ></Input>
+                    </div>
+                  </div>
+                  <TextArea
+                    placeholder="როლი თანამდებობაზე და ზოგადი აღწერა"
+                    {...register(`experience.${index}.description`, {
+                      required: true,
+                    })}
+                    style={{ borderColor: getBorderColor(index) }}
+                  ></TextArea>
+                  <div
+                    style={{
+                      marginTop: "56px",
+                      height: "1px",
+                      width: "100%",
+                      backgroundColor: "#bcbcbc",
+                    }}
+                  ></div>
+                  <div
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    {index !== 0 && (
+                      <DeleteButton type="button" onClick={() => remove(index)}>
+                        გამოცდილების წაშლა
+                      </DeleteButton>
+                    )}
+                  </div>
+                </div>
+              ))}
+
+              <LightSkyButton
+                style={{ marginTop: fields.length !== 1 ? "-48px" : "0px" }}
+                type="button"
+                onClick={() =>
+                  append({
+                    position: "",
+                    employer: "",
+                    startingDate: "mm/dd/yyyy",
+                    finishingDate: "mm/dd/yyyy",
+                    description: "",
+                  })
+                }
+              >
+                მეტი გამოცდილების დამატება
+              </LightSkyButton>
+            </section>
+            <Footer>
+              <Link to={"/PersonalInfo"}>
+                <BlueButton type="button">უკან</BlueButton>
+              </Link>
+              <BlueButton>შემდეგი</BlueButton>
+            </Footer>
+          </MainDiv>
+        </form>
+      </FormProvider>
+      <CVcomponent />
+    </div>
+  );
 }
 
-export default PersonalInfo;
+const MainDiv = styled.div`
+  width: 1078px;
+  background-color: #f9f9f9;
+  padding: 45px 149px 65px 48px;
+  min-height: 100vh;
+`;
+
+const TextArea = styled.textarea`
+  height: 123px;
+  padding-left: 16px;
+  padding-top: 13px;
+  resize: none;
+  border: solid 1px;
+  border-radius: 4px;
+  outline: none;
+`;
+
+const Footer = styled.footer`
+  padding-left: 102px;
+  margin-top: 115px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DeleteButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  border: none;
+  background-color: red;
+  font-size: 14px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #fff;
+  padding: 14px 22px;
+  cursor: pointer;
+`;
+
+export const LightSkyButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  border: none;
+  background-color: #62a1eb;
+  font-size: 14px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #fff;
+  padding: 14px 22px;
+  cursor: pointer;
+`;
+
+export const BlueButton = styled(LightSkyButton)`
+  background-color: #6b40e3;
+  padding: 10px 18px;
+`;
